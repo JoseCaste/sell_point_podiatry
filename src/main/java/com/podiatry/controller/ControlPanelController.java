449d792f0f158f26e14784a9ae83ac6a8e8f662d
JoseCaste <jotacastellanosguz@gmail.com>
2021-07-08 15:48:16 -0500

    Change way to get img resource, before was get from com.podiatry.stuff and now it´s gets from C:/img

--- src/main/java/com/podiatry/controller/ControlPanelController.java
+++ src/main/java/com/podiatry/controller/ControlPanelController.java
@@ -1,19 +1,17 @@
 package com.podiatry.controller;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
+
+import java.io.File;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
-
-import javax.imageio.ImageIO;
+import java.util.Random;
 import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.podiatry.PodiatryApplication;
 import com.podiatry.model.Product;
 import com.podiatry.repository.ProductRepository;
 
@@ -39,21 +37,15 @@ public class ControlPanelController {
 	@GetMapping("/uploadImage")
 	public String saveProduct() {
 		String [] names = {"Pinza","Crema verde","Aceite","Plantilla","Soporte","Kit","Exfoliante"};
-		int [] price = {125,500,250,70,1500,180,96}; 
 		List<Product> products= new ArrayList<>();
 		for (int i=0;i<7;i++) {
 			try {
-				String url=String.format("/com/podiatry/stuff/%d.jpg",i);
-				String type="jpg";
-				BufferedImage img = ImageIO.read(PodiatryApplication.class.getResource(url));
-				ByteArrayOutputStream bos = new ByteArrayOutputStream();
-				ImageIO.write(img, type, bos);
-	            byte[] imageBytes = bos.toByteArray();
+				File file = new File(String.format("C:\\img\\%d.jpg",i));
+				byte[] imageBytes = Files.readAllBytes(file.toPath());
 	            Base64.Encoder encoder = Base64.getEncoder();
 	            String encoded = encoder.encodeToString(imageBytes);
-	            System.out.println(new Product(""+i,i,i,encoded).toString());
-	            
-				products.add(new Product(names[i],i,price[i],encoded));
+	            Random random= new Random();
+				products.add(new Product(names[i],random.nextInt(200-10)+10, random.nextInt(200-10)+10,encoded));
 			} catch (Exception e) {
 				// TODO: handle exception
 				e.printStackTrace();
