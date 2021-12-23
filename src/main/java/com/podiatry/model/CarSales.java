5619f01e081e5f443dbe2b89cb142129b0ff82c3
JoseCaste <jotacastellanosguz@gmail.com>
2021-12-22 22:22:49 -0600

    Eliminación de importaciones no usadas

--- src/main/java/com/podiatry/model/CarSales.java
+++ src/main/java/com/podiatry/model/CarSales.java
@@ -1,8 +1,5 @@
 package com.podiatry.model;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,11 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
