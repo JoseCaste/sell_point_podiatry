51ec7c3785074047741028dfff39e04a63b0938b
José Castellanos <jotacastellanosguz@gmail.com>
2022-03-01 23:42:40 -0600

    Load all dates and json parser to fronted

--- src/main/java/com/podiatry/controller/adminController/AdministratorController.java
+++ src/main/java/com/podiatry/controller/adminController/AdministratorController.java
@@ -1,4 +1,47 @@
 package com.podiatry.controller.adminController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.podiatry.model.Citas;
+import com.podiatry.pojo.DateCalendarData;
+import com.podiatry.repository.CitasRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
 public class AdministratorController {
+
+    private CitasRepository citasRepository;
+    public AdministratorController(CitasRepository citasRepository){
+        this.citasRepository= citasRepository;
+    }
+    @GetMapping("/dates")
+    public String showDates(Model model){
+        List<Citas> citas= this.citasRepository.findAll();
+        try {
+            String json = mapObjectToFullCalendar(citas);
+            model.addAttribute("citasJson",json);
+        }catch (JsonProcessingException e){
+            model.addAttribute("error", "Hubo un error al recuperar las citas");
+        }
+        return "admin/dates";
+    }
+
+    private String mapObjectToFullCalendar(List<Citas> citas) throws JsonProcessingException {
+        List<DateCalendarData> parseDate = citas.stream().map(cita->{
+            LocalDateTime date= LocalDateTime.of(cita.getDate(),cita.getTime());
+            return new DateCalendarData(cita.getId(),String.format("%s %s - %s",cita.getPacientName(),cita.getLastName(),cita.getPlace()),date.toString());
+        }).collect(Collectors.toCollection(ArrayList::new));
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new Hibernate5Module());
+        return mapper.writeValueAsString(parseDate);
+    }
 }
